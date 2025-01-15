6227616c86be2c20fd498ce880e5dff3b183115e
zSukita <lucassilva1001@hotmail.com>
2025-01-15 18:31:01 -0300

    atialização

--- keypad_1.c
+++ keypad_1.c
@@ -2,14 +2,14 @@
 #include "pico/stdlib.h"
 #include "hardware/timer.h"
 
-//define o LED de saída
-#define GPIO_LED 18
+#define GPIO_LED_BLUE 11
+#define GPIO_LED_RED 12
+#define GPIO_LED_GREEN 13
+#define GPIO_BUZZER 21
 
-//define os pinos do teclado com as portas GPIO
-uint columns[4] = {4, 3, 2, 1}; 
-uint rows[4] = {8, 7, 6, 5};
+uint columns[4] = {6, 7, 8, 9}; 
+uint rows[4] = {2, 3, 4, 5};
 
-//mapa de teclas
 char KEY_MAP[16] = {
     '1', '2' , '3', 'A',
     '4', '5' , '6', 'B',
@@ -23,23 +23,19 @@ char _matrix_values[16];
 uint all_columns_mask = 0x0;
 uint column_mask[4];
 
-//imprimir valor binário
 void imprimir_binario(int num) {
- int i;
- for (i = 31; i >= 0; i--) {
-  (num & (1 << i)) ? printf("1") : printf("0");
- }
+    int i;
+    for (i = 31; i >= 0; i--) {
+        (num & (1 << i)) ? printf("1") : printf("0");
+    }
 }
 
-//inicializa o keypad
 void pico_keypad_init(uint columns[4], uint rows[4], char matrix_values[16]) {
-
     for (int i = 0; i < 16; i++) {
         _matrix_values[i] = matrix_values[i];
     }
 
     for (int i = 0; i < 4; i++) {
-
         _columns[i] = columns[i];
         _rows[i] = rows[i];
 
@@ -56,7 +52,6 @@ void pico_keypad_init(uint columns[4], uint rows[4], char matrix_values[16]) {
     }
 }
 
-//coleta o caracter pressionado
 char pico_keypad_get_key(void) {
     int row;
     uint32_t cols;
@@ -65,7 +60,7 @@ char pico_keypad_get_key(void) {
     cols = gpio_get_all();
     cols = cols & all_columns_mask;
     imprimir_binario(cols);
-    
+
     if (cols == 0x0) {
         return 0;
     }
@@ -75,17 +70,14 @@ char pico_keypad_get_key(void) {
     }
 
     for (row = 0; row < 4; row++) {
-
         gpio_put(_rows[row], 1);
-
         busy_wait_us(10000);
-
         cols = gpio_get_all();
         gpio_put(_rows[row], 0);
         cols = cols & all_columns_mask;
         if (cols != 0x0) {
             break;
-        }   
+        }    
     }
 
     for (int i = 0; i < 4; i++) {
@@ -109,25 +101,41 @@ char pico_keypad_get_key(void) {
     }
 }
 
-//função principal
 int main() {
     stdio_init_all();
     pico_keypad_init(columns, rows, KEY_MAP);
     char caracter_press;
-    gpio_init(GPIO_LED);
-    gpio_set_dir(GPIO_LED, GPIO_OUT);
+
+    gpio_init(GPIO_LED_BLUE);
+    gpio_set_dir(GPIO_LED_BLUE, GPIO_OUT);
+    gpio_init(GPIO_LED_RED);
+    gpio_set_dir(GPIO_LED_RED, GPIO_OUT);
+    gpio_init(GPIO_LED_GREEN);
+    gpio_set_dir(GPIO_LED_GREEN, GPIO_OUT);
+    gpio_init(GPIO_BUZZER);
+    gpio_set_dir(GPIO_BUZZER, GPIO_OUT);
 
     while (true) {
         caracter_press = pico_keypad_get_key();
         printf("\nTecla pressionada: %c\n", caracter_press);
 
-        //Avaliação de caractere para o LED
-        if (caracter_press=='B')
-        {
-            gpio_put(GPIO_LED,true);
-        }else
-        {
-            gpio_put(GPIO_LED,false);
+        if (caracter_press == 'A') {
+            gpio_put(GPIO_LED_BLUE, true);
+        } else if (caracter_press == 'B') {
+            gpio_put(GPIO_LED_RED, true);
+        } else if (caracter_press == 'C') {
+            gpio_put(GPIO_LED_GREEN, true);
+        } else if (caracter_press == 'D') {
+            gpio_put(GPIO_LED_BLUE, true);
+            gpio_put(GPIO_LED_RED, true);
+            gpio_put(GPIO_LED_GREEN, true);
+        } else if (caracter_press == '1') {
+            gpio_put(GPIO_BUZZER, true);
+        } else {
+            gpio_put(GPIO_LED_BLUE, false);
+            gpio_put(GPIO_LED_RED, false);
+            gpio_put(GPIO_LED_GREEN, false);
+            gpio_put(GPIO_BUZZER, false);
         }
         busy_wait_us(500000);
     }
