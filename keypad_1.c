4fcf529dc00fe14b391e1c121d5c9e2195d15c34
Erick <persona0rcc@gmail.com>
2025-01-15 20:21:20 -0300

    Main+EstruturaIfElse+LogControle

--- keypad_1.c
+++ keypad_1.c
@@ -11,5 +11,21 @@ void imprimir_binario(int num) {
 }
 
 int main() {
-    
+    char caracter_press;
+
+    while (true) {
+        if (caracter_press == 'A') {
+            //led
+        } else if (caracter_press == 'B') {
+            //led
+        } else if (caracter_press == 'C') {
+           //led
+        } else if (caracter_press == 'D') {
+            //3leds
+        } else if (caracter_press == '1') {
+            //buzzer
+        } else {
+            //desligar leds e buzzer
+        }
+    }
 }
