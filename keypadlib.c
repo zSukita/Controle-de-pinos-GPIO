b4a0941cfa4119db83a4bbc0d7496149c94cbfcb
Jobson Almeida <jobson.ra@outlook.com>
2025-01-17 09:14:54 -0300

    chore: definir macro que representa a primeira gpio do teclado

--- keypadlib.c
+++ keypadlib.c
@@ -1,16 +1,19 @@
 #include "pico/stdlib.h"
 #include <stdio.h>
 
+// referente a primeira GPIO destinada ao teclado, localizada no pino 2
+#define GPIO_STARTER_PIN 2
+
 void keypad_init()
 {
-  // inicializa as GPIO's - GPIO2 à GPIO9. O número 2 se refere a primeira GPIO no pino 2
-  gpio_init_mask((0xFF << 2));
+  // inicializa as GPIO's - GPIO2 à GPIO9
+  gpio_init_mask((0xFF << GPIO_STARTER_PIN));
 
   // define as linhas (rows) como saída - GPIOs de 2 à 5
-  gpio_set_dir_out_masked((0xF << 2));
+  gpio_set_dir_out_masked((0xF << GPIO_STARTER_PIN));
 
   // define o nível lógico das saídas (linhas)
-  gpio_put_masked((0xF << 2), (0x0 << 2));
+  gpio_put_masked((0xF << GPIO_STARTER_PIN), (0x0 << GPIO_STARTER_PIN));
 
   // define as colunas como pull down
   gpio_pull_down(6); // GPIO6
@@ -18,26 +21,25 @@ void keypad_init()
   gpio_pull_down(8); // GPIO8
   gpio_pull_down(9); // GPIO9
 
-  //
-  printf("estado das GPIOs: ");
-  for (size_t a = 2; a < 10; a++)
-  {
-    printf("%d", gpio_get(a));
-  }
-  printf("\ndireção das GPIOs: ");
-  for (size_t a = 2; a < 10; a++)
-  {
-    printf("%d", gpio_get_dir(a));
-  }
-  printf("\nGPIOs que são pulled down: ");
-  for (size_t a = 2; a < 10; a++)
-  {
-    printf("%d", gpio_is_pulled_down(a));
-  }
-  printf("\nGPIOs que são pulled up: ");
-  for (size_t a = 2; a < 10; a++)
-  {
-    printf("%d", gpio_is_pulled_up(a));
-  }
-  printf("\n");
+  // printf("estado das GPIOs: ");
+  // for (size_t a = GPIO_STARTER_PIN; a < 10; a++)
+  // {
+  //   printf("%d", gpio_get(a));
+  // }
+  // printf("\ndireção das GPIOs: ");
+  // for (size_t a = GPIO_STARTER_PIN; a < 10; a++)
+  // {
+  //   printf("%d", gpio_get_dir(a));
+  // }
+  // printf("\nGPIOs que são pulled down: ");
+  // for (size_t a = GPIO_STARTER_PIN; a < 10; a++)
+  // {
+  //   printf("%d", gpio_is_pulled_down(a));
+  // }
+  // printf("\nGPIOs que são pulled up: ");
+  // for (size_t a = GPIO_STARTER_PIN; a < 10; a++)
+  // {
+  //   printf("%d", gpio_is_pulled_up(a));
+  // }
+  // printf("\n");
 }
