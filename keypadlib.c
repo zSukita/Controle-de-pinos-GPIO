ef1782a814106f916fa74266c5c9995d67ac26b9
Jobson Almeida <jobson.ra@outlook.com>
2025-01-17 08:57:12 -0300

    build: inicializar a mascara das gpios relacionadas ao teclado

--- keypadlib.c
+++ keypadlib.c
@@ -1,8 +1,16 @@
 #include "pico/stdlib.h"
+#include <stdio.h>
 
 void keypad_init()
 {
     // inicializa as GPIO's - GPIO2 à GPIO9
     gpio_init_mask((0xFF << 2));
 
+
+
+
+  for (size_t a = 2; a < 10; a++)
+    {
+        printf("%d", gpio_get(a));
+    }
 }
