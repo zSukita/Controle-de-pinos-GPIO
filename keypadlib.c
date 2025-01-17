78693f07d189b8952974b5b2bac1fcc74f8200cc
Jobson Almeida <jobson.ra@outlook.com>
2025-01-17 09:03:35 -0300

    build: definir direção das gpios utilizando a mascara

--- keypadlib.c
+++ keypadlib.c
@@ -3,14 +3,22 @@
 
 void keypad_init()
 {
-    // inicializa as GPIO's - GPIO2 à GPIO9
-    gpio_init_mask((0xFF << 2));
+  // inicializa as GPIO's - GPIO2 à GPIO9. O número 2 se refere a primeira GPIO no pino 2
+  gpio_init_mask((0xFF << 2));
 
+  // define as linhas (rows) como saída - GPIOs de 2 à 5
+  gpio_set_dir_out_masked((0xF << 2));
 
-
-
+  //
+  printf("estado das GPIOs: ");
   for (size_t a = 2; a < 10; a++)
-    {
-        printf("%d", gpio_get(a));
-    }
+  {
+    printf("%d", gpio_get(a));
+  }
+  printf("\ndireção das GPIOs: ");
+  for (size_t a = 2; a < 10; a++)
+  {
+    printf("%d", gpio_get_dir(a));
+  }
+  printf("\n");
 }
